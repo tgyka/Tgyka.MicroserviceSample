079a47a693225caeaad47ddb4733c5956b0ab427
tgyka <tugaykaramustafa@windowslive.com>
2023-10-12 13:39:06 +0300

    product in search service implemented

---
+++ Services/SearchService/Tgyka.Microservice.SearchService/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MssqlRestApi.Base.Controller;
+using Tgyka.Microservice.SearchService.Model.Dtos;
+using Tgyka.Microservice.SearchService.Services.Abstractions;
+
+namespace Tgyka.Microservice.SearchService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : TgykaMicroserviceControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> GetProducts(string searchString,int page,int size,bool priceIsDescending = false)
+        {
+            return ApiActionResult<List<ProductResponseDto>>(await _productService.GetProducts(searchString, page, size, priceIsDescending));
+        }
+    }
+}
--- Services/SearchService/Tgyka.Microservice.SearchService/Services/Implementations/ProductService.cs
+++ Services/SearchService/Tgyka.Microservice.SearchService/Services/Implementations/ProductService.cs
@@ -1,8 +1,10 @@
 ﻿using Nest;
+using System.Reflection.Metadata;
 using Tgyka.Microservice.Base.Model.ApiResponse;
 using Tgyka.Microservice.SearchService.Model.Dtos;
 using Tgyka.Microservice.SearchService.Services.Abstractions;
 using Tgyka.Microservice.SearchService.Settings;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Tgyka.Microservice.SearchService.Services.Implementations
 {
@@ -19,9 +21,9 @@ namespace Tgyka.Microservice.SearchService.Services.Implementations
                         .DefaultIndex(elasticSetttings.DefaultIndex);
             _client = new ElasticClient(_connectionSettings);
 
-            if (!_client.Indices.Exists("esearchitems").Exists)
+            if (!_client.Indices.Exists("productsearchs").Exists)
             {
-                _client.Indices.Create("esearchitems",
+                _client.Indices.Create("productsearchs",
                      index => index.Map<ProductResponseDto>(
                           x => x
                          .AutoMap()
@@ -29,9 +31,9 @@ namespace Tgyka.Microservice.SearchService.Services.Implementations
             }
         }
 
-        public async Task<ApiResponseDto<ProductResponseDto>> GetProducts(string searchString,int page,int size)
+        public async Task<ApiResponseDto<List<ProductResponseDto>>> GetProducts(string searchString,int page,int size,bool priceIsDescending)
         {
-            var data = await _client.SearchAsync<ProductResponseDto>(s => s
+            var data = (await _client.SearchAsync<ProductResponseDto>(s => s
                 .From((page - 1) * size)
                 .Size(size)
                 .Query(q => q
@@ -45,9 +47,22 @@ namespace Tgyka.Microservice.SearchService.Services.Implementations
                     )
                 )
                 .Sort(sort => sort
-                    .Field(f => f.Field(p => p.Price).Order(SortOrder.Ascending))
-                ));
-            return ApiResponseDto<ProductResponseDto>(200, data);
+                    .Field(f => f.Field(p => p.Price).Order(priceIsDescending ? SortOrder.Descending : SortOrder.Ascending))
+                ))).Documents.ToList();
+
+            return ApiResponseDto<List<ProductResponseDto>>.Success(200, data);
+        }
+
+        public async Task<ApiResponseDto<ProductResponseDto>> CreateProduct(ProductResponseDto request)
+        {
+            await _client.IndexDocumentAsync(request);
+            return ApiResponseDto<ProductResponseDto>.Success(200, request);
+        }
+
+        public async Task<ApiResponseDto<bool>> DeleteProduct(int productId)
+        {
+            var result = await _client.DeleteAsync<ProductResponseDto>(productId.ToString());
+            return ApiResponseDto<bool>.Success(200, result.IsValid);
         }
     }
 }
