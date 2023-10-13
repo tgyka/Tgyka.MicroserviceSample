1afa55ae42733d25d71f3baa78977891e72dd00b
tgyka <tugaykaramustafa@windowslive.com>
2023-10-13 17:14:20 +0300

    order service initialized

--- Services/SearchService/Tgyka.Microservice.SearchService/Services/Abstractions/IProductService.cs
+++ Services/SearchService/Tgyka.Microservice.SearchService/Services/Abstractions/IProductService.cs
@@ -1,6 +1,12 @@
-﻿namespace Tgyka.Microservice.SearchService.Services.Abstractions
+﻿using Tgyka.Microservice.Base.Model.ApiResponse;
+using Tgyka.Microservice.SearchService.Model.Dtos;
+
+namespace Tgyka.Microservice.SearchService.Services.Abstractions
 {
     public interface IProductService
     {
+        Task<ApiResponseDto<ProductResponseDto>> CreateProduct(ProductResponseDto request);
+        Task<ApiResponseDto<bool>> DeleteProduct(int productId);
+        Task<ApiResponseDto<List<ProductResponseDto>>> GetProducts(string searchString, int page, int size, bool priceIsDescending);
     }
 }
