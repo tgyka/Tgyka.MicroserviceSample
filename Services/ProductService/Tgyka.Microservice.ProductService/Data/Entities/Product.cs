f3f09c2cfd87eaf5841b44d84341d3efacc54795
tgyka <tugaykaramustafa@windowslive.com>
2023-10-24 17:26:17 +0300

    masstransit added to product service

--- Services/ProductService/Tgyka.Microservice.ProductService/Data/Entities/Product.cs
+++ Services/ProductService/Tgyka.Microservice.ProductService/Data/Entities/Product.cs
@@ -7,6 +7,7 @@ namespace Tgyka.Microservice.ProductService.Data.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
+        public int Stock { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
     }
