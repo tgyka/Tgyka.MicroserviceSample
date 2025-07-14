41b64eb17af8e26e90aa6ece2696ac0ec40362fd
Tugay <42774349+tgyka@users.noreply.github.com>
2025-07-14 16:02:40 +0300

    Add update and delete endpoints for role permissions

--- Services/IdentityService/Tgyka.Microservice.IdentityService/Services/Abstractions/IRoleService.cs
+++ Services/IdentityService/Tgyka.Microservice.IdentityService/Services/Abstractions/IRoleService.cs
@@ -5,7 +5,13 @@ namespace Tgyka.Microservice.IdentityService.Services.Abstractions
     public interface IRoleService
     {
         Task<ApiResponse<string>> CreateRole(string roleName);
+        Task<ApiResponse<string>> UpdateRole(string roleName, string newRoleName);
+        Task<ApiResponse<string>> DeleteRole(string roleName);
+
         Task<ApiResponse<string>> AssignRoleToUser(string userId, string roleName);
+
         Task<ApiResponse<string>> AddPermissionToRole(string roleName, string permission);
+        Task<ApiResponse<string>> UpdatePermission(string roleName, string oldPermission, string newPermission);
+        Task<ApiResponse<string>> DeletePermission(string roleName, string permission);
     }
 }
